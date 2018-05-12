50c9ee1c178e922c1c4dea28ed9f87cd8328f6b4
Kyle Patterson <patterkyle@gmail.com>
2018-05-12 13:57:50 -0500

    change state with key presses

--- tic-tac-toe.rkt
+++ tic-tac-toe.rkt
@@ -5,45 +5,51 @@
 
 (provide game-over?)
 
-(define name     "tic-tac-toe")
-(define frame-w  400)
-(define frame-h  300)
-
-;game state
-(struct gs (board
-            next-move)
-  #:transparent
-  #:mutable)
+(define name       "tic-tac-toe")
+(define frame-w    400)
+(define frame-h    300)
+(define first-move (list-ref '(x o) (random 2)))
 
 (define (empty-board)
   (make-vector 9 empty))
 
+;game state
+(struct gs (board
+            next-move
+            over?/winner) ; #f if game is ongoing, 'x or 'y if there's a winner
+  #:transparent
+  #:mutable)
+
+(define game-state (gs (empty-board)
+                       first-move
+                       #f))
+
 (define (game-over? board)
   (match board
-    [(vector a a a
-             _ _ _
-             _ _ _) #:when (not (empty? a)) a]
-    [(vector _ _ _
-             a a a
-             _ _ _) #:when (not (empty? a)) a]
-    [(vector _ _ _
-             _ _ _
-             a a a) #:when (not (empty? a)) a]
-    [(vector a _ _
-             a _ _
-             a _ _) #:when (not (empty? a)) a]
-    [(vector _ a _
-             _ a _
-             _ a _) #:when (not (empty? a)) a]
-    [(vector _ _ a
-             _ _ a
-             _ _ a) #:when (not (empty? a)) a]
-    [(vector a _ _
-             _ a _
-             _ _ a) #:when (not (empty? a)) a]
-    [(vector _ _ a
-             _ a _
-             a _ _) #:when (not (empty? a)) a]
+    [(or (vector a a a
+                 _ _ _
+                 _ _ _)
+         (vector _ _ _
+                 a a a
+                 _ _ _)
+         (vector _ _ _
+                 _ _ _
+                 a a a)
+         (vector a _ _
+                 a _ _
+                 a _ _)
+         (vector _ a _
+                 _ a _
+                 _ a _)
+         (vector _ _ a
+                 _ _ a
+                 _ _ a)
+         (vector a _ _
+                 _ a _
+                 _ _ a)
+         (vector _ _ a
+                 _ a _
+                 a _ _)) #:when (not (empty? a)) a]
     [_ #f]))
 
 (define (draw-cell val w h)
@@ -82,18 +88,33 @@
 (define (handle-event event)
   (void))
 
-(define (handle-char-event event)
-  (void))
+(define (update-cell! state i)
+  (vector-set! (gs-board state) i (gs-next-move state))
+  (set-gs-next-move! state (if (eq? (gs-next-move state) 'x)
+                               'o
+                               'x)))
+
+(define (handle-char-event event canvas)
+  (match (send event get-key-code)
+    [a #:when (and (not (empty? a))
+                   (not (symbol? a))
+                   (char-numeric? a))
+       (let ([num (string->number (string a))])
+         (when (< 0 num 10)
+           (update-cell! game-state (sub1 num))
+           (send canvas refresh)))]
+    [_ (void)]))
 
 (define (paint-callback canvas dc)
-  (draw-pict (game-board b2 frame-w frame-h)
+  (send dc set-smoothing 'aligned)
+  (draw-pict (game-board (gs-board game-state) frame-w frame-h)
              dc 0 0))
 
 (define game-canvas% (class canvas%
                        (define/override (on-event event)
                          (handle-event event))
                        (define/override (on-char event)
-                         (handle-char-event event))
+                         (handle-char-event event this))
                        (super-new)))
 
 (new game-canvas%
@@ -103,4 +124,4 @@
 (define (main)
   (send frame show #t))
 
-(main)
+;; (main)
